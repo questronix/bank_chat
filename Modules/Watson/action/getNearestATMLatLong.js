d6201d47360402fc9ccc638403d107ca69827771
jbpanganiban2 <jbpanganiban2@up.edu.ph>
2018-07-12 14:06:11 +0800

    Added status when using current location

--- Modules/Watson/action/getNearestATMLatLong.js
+++ Modules/Watson/action/getNearestATMLatLong.js
@@ -1,5 +1,41 @@
+const TAG = '[getNearestATMPlace]';
 const atm = require('../../ATM/model/ATM');
+const async = require('async');
+const err = require('../../Common/service/Errors');
+const logger = require('../../Common/service/Logger');
+
 
 module.exports.run = (context)=>{
-  return atm.getNearestATM(context.context.lat, context.context.lng);
+  const ACTION = '[run]';
+  logger.log('info', TAG + ACTION, context);
+
+  return new Promise((resolve, reject)=>{
+    async.auto({
+      getNearestATM : function(callback){
+            atm.getNearestATM(context.context.lat, context.context.lng).then(data=>{
+              callback(null, data);
+            }).catch(error=>{
+              logger.log('error', TAG + ACTION, error);
+              callback(error);
+            });
+      },
+      addATMStatus : ['getNearestATM', function(result, callback){
+        if(result.getNearestATM.length > 0 || result.getNearestATM.response.statusCode == 200){
+          //loop here add random status
+          let output = result.getNearestATM.map(elem=>{
+            elem.status = Math.round(Math.random());  //random(1);
+            return elem;
+          });
+
+          callback(null, output);
+        }else{
+          let er = err.raise('STATUS_PROBLEM');
+            callback(er);
+        }
+      }]
+    }, function(err, result){
+      if(err) reject(err);
+      resolve(result.addATMStatus);
+    });
+  });
 };
