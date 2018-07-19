cce1b33b9a2fe691ff250b172fa69c8378b653d9
jbpanganiban2 <jbpanganiban2@up.edu.ph>
2018-07-19 14:12:49 +0800

    Fixed loan details

--- Modules/Loan/model/Loan.js
+++ Modules/Loan/model/Loan.js
@@ -4,7 +4,7 @@ const err = require('../../Common/service/Errors');
 const logger = require('../../Common/service/Logger');
 
 const TABLE1_NAME = 'loan';
-// const TABLE2_NAME = 'loanreqs';
+const TABLE2_NAME = 'loanreqs';
 
 const PARAMS = 'PARAMS';
 
@@ -21,8 +21,9 @@ module.exports.getLoanDetails = () => {
     return new Promise((resolve, reject) => {
         let cols = TABLE_COLUMNS;
         let sql = `
-            // SELECT ${Object.keys(cols).join(',')} FROM ${TABLE1_NAME} INNER JOIN ${TABLE2_NAME} ON ${TABLE1_NAME}.loan_id = ${TABLE2_NAME}.loan_id`;
-            SELECT ${Object.keys(cols).join(',')} FROM ${TABLE1_NAME}`;
+            SELECT loan.id, loan.loan_id, loan.name FROM ${TABLE1_NAME} INNER JOIN ${TABLE2_NAME} ON ${TABLE1_NAME}.loan_id = ${TABLE2_NAME}.loan_id`;
+            //SELECT ${Object.keys(cols).join(',')} FROM ${TABLE1_NAME} INNER JOIN ${TABLE2_NAME} ON ${TABLE1_NAME}.loan_id = ${TABLE2_NAME}.loan_id
+            // SELECT ${Object.keys(cols).join(',')} FROM ${TABLE1_NAME}
         db.execute(sql,[]).then(rows=>{
             resolve(JSON.parse(JSON.stringify(rows)));
         }).catch(error=>{
@@ -32,19 +33,19 @@ module.exports.getLoanDetails = () => {
     });
 };
 
-// module.exports.getSpecificDetail = (inputName) => {
-//     const ACTION = '[getSpecificDetail]';
-//     return new Promise((resolve, reject) => {
-//         let cols = TABLE_COLUMNS;
-//         let sql = `
-//             SELECT ${Object.keys(cols).join(',')} FROM ${TABLE1_NAME} INNER JOIN ${TABLE2_NAME} ON ${TABLE1_NAME}.loan_id = ${TABLE2_NAME}.loan_id WHERE name = ?`;
+module.exports.getSpecificDetail = (inputName) => {
+    const ACTION = '[getSpecificDetail]';
+    return new Promise((resolve, reject) => {
+        let cols = TABLE_COLUMNS;
+        let sql = `
+            SELECT ${Object.keys(cols).join(',')} FROM ${TABLE1_NAME} INNER JOIN ${TABLE2_NAME} ON ${TABLE1_NAME}.loan_id = ${TABLE2_NAME}.loan_id WHERE ${TABLE1_NAME}.name = ?`;
 
-//         db.execute(sql,[inputName]).then(rows=>{
-//             resolve(JSON.parse(JSON.stringify(rows)));
-//         }).catch(error=>{
-//             logger.log('error', TAG + ACTION, error);
-//             reject(err.raise('INTERNAL_SERVER_ERROR', error));
-//         });
-//     });
-// };
+        db.execute(sql,[inputName]).then(rows=>{
+            resolve(JSON.parse(JSON.stringify(rows)));
+        }).catch(error=>{
+            logger.log('error', TAG + ACTION, error);
+            reject(err.raise('INTERNAL_SERVER_ERROR', error));
+        });
+    });
+};
 
